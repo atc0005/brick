3c9432c0aad22448c5465093a48a1b207b942a84
Adam Chalkley <atc0005@users.noreply.github.com>
2022-09-04 07:24:45 -0500

    Add missing cmd doc files
    Add "package" doc files to resolve revive `package-comment`
    linting error surfaced by recent linter update.

---
+++ cmd/es/doc.go
@@ -0,0 +1,30 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/brick
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// es is a CLI application used to list and optionally terminate user sessions
+// for a specific username. This tool is intended for quick troubleshooting or
+// as an optional replacement for logging into the admin UI to terminate user
+// sessions for a specific username.
+//
+// See our [GitHub repo]:
+//
+//   - to review documentation (including examples)
+//   - for the latest code
+//   - to file an issue or submit improvements for review and potential
+//     inclusion into the project
+//
+// [GitHub repo]: https://github.com/atc0005/brick
+package main
---
+++ cmd/ezproxy/doc.go
@@ -0,0 +1,30 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/brick
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// The ezproxy binary provided by this project is used as a stand-in for the
+// real ezproxy application during brick's development. This binary randomly
+// exits with specific exit codes in order to exercise brick's handling of
+// external calls to the real/official ezproxy binary.
+//
+// See our [GitHub repo]:
+//
+//   - to review documentation (including examples)
+//   - for the latest code
+//   - to file an issue or submit improvements for review and potential
+//     inclusion into the project
+//
+// [GitHub repo]: https://github.com/atc0005/brick
+package main
